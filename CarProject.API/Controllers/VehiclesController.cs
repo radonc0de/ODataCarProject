638e83e79d97dd805daa980fd8504757679e035f
Keith Lehman <klehman@langan.com>
2022-08-23 15:49:21 -0400

    some more changes

---
+++ CarProject.API/Controllers/VehiclesController.cs
@@ -0,0 +1,24 @@
+﻿using CarProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarProject.API.Controllers
+{
+    public class VehiclesController
+    {
+        private CarProjectDbContext _ctx = new CarProjectDbContext();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_ctx.Driver);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
